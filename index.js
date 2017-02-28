be7e97896206db41ed88ae00b2c15355ae904a34
udaysphere <uday@practiceconsulting.com>
2017-02-28 18:15:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-var AWS = require('aws-sdk');
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+//var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
-exports.handler = function(event, context) {
-  context.succeed("hello uday again");
-}
+var poolData = {
+       UserPoolId : 'us-west-2_zGS9RytY0', // Your user pool id here
+       ClientId : '6jj6utd8uaf265bep14jaqqlh0' // Your client id here
+   };
+   var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+   var attributeList = [];
+
+   var dataEmail = {
+       Name : 'email',
+       Value : 'uday@practiceconsulting.com'
+   };
+
+   var dataPhoneNumber = {
+       Name : 'phone_number',
+       Value : '+919490217947'
+   };
+   var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+   var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
+
+   attributeList.push(attributeEmail);
+   attributeList.push(attributePhoneNumber);
+
+   userPool.signUp('demo', 'password', attributeList, null, function(err, result){
+       if (err) {
+           console.log(err);
+           return;
+       }
+       var cognitoUser = result.user;
+       console.log('user name is ' + cognitoUser.getUsername());
+   });
